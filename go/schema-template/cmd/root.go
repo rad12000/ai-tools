1eeea006fa7cce68065dda5cc30cf147bb4218e6
chad-bekmezian-snap <chad.bekmezian@snapone.com>
2026-04-01 11:34:34 -0600

    fix: update command description

--- go/schema-template/cmd/root.go
+++ go/schema-template/cmd/root.go
@@ -18,13 +18,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "schema-template",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Renders go template files, validating their inputs against defined JSON Schema files, which may be written in YAML or JSON.",
+	Long:  "Renders go template files, validating their inputs against defined JSON Schema files, which may be written in YAML or JSON.",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
